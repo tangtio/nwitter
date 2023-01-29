ad9c305043905874c4d1264678d0122fce7fa898
kimyewon <tangtio@naver.com>
2023-01-30 00:57:44 +0900

    6.1 Styles

---
+++ src/routes/Auth.js
@@ -0,0 +1,30 @@
+import { authService, firebaseInstance } from "fbase";
+import React, {useState} from "react";
+import AuthForm from "components/AuthForm";
+
+const Auth = ()=> {
+ 
+const onSocialClick =async(event) => {
+    const {
+        target: {name},
+     } = event; 
+     let provider;
+     if(name==="google"){
+        provider = new firebaseInstance.auth.GoogleAuthProvider();
+     } else if (name==="github"){
+        provider = new firebaseInstance.auth.GithubAuthProvider();
+     }
+     await authService.signInWithPopup(provider);
+    
+     };
+return(
+<div>
+    <AuthForm />
+    <div>
+        <button onClick={onSocialClick} name="google">Continue with Google</button>
+        <button onClick={onSocialClick} name="github">Continue with Github</button>
+    </div>
+
+</div>);
+};
+export default Auth;
---
+++ src/routes/Home.js
@@ -0,0 +1,33 @@
+import React, {useEffect, useState} from "react";
+import { dbService, storageService } from "fbase";
+import Nweet from "components/Nweet";
+import NweetFactory from "components/NweetFactory";
+
+const Home = ({userObj})=> {
+    const [nweets, setNweets] = useState([]);
+
+    useEffect(()=>{ 
+        dbService.collection("nweets").onSnapshot((snapshot)=>{
+            const nweetArray = snapshot.docs.map(doc=>({
+                id:doc.id, 
+                ...doc.data(),
+            }));
+            setNweets(nweetArray);
+        });
+    }, []);
+    
+   return (
+        <div>
+            <NweetFactory userObj={userObj}></NweetFactory>
+            <div>
+            {nweets.map((nweet) => (
+                <Nweet key={nweet.id} 
+                nweetObj={nweet} 
+                isOwner={nweet.creatorId ===userObj.uid}
+                />
+        ))}
+        </div>
+        </div>
+        );
+};
+export default Home;
